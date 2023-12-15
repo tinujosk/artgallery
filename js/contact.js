83e60187c2a1025fb5e295b1811b002b652f2b6f
Tinu Jos K <josk.tinu@gmail.com>
2023-12-15 14:15:33 -0500

    contact us page completion

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#contactForm").submit(function (e) {
+    e.preventDefault(); // Prevent the form from submitting normally
+
+    // Validate form fields
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const phone = $("#phone").val();
+    const message = $("#message").val();
+
+    if (name === "" || email === "" || phone === "" || message === "") {
+      alert("Please fill in all fields."); // Display an alert if any field is empty
+    } else {
+      // If all fields are filled, show "Message sent" popup with a slide-in effect
+      $("#popup").slideDown(700).delay(2000).slideUp(700);
+    }
+  });
+});
